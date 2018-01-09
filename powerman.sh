06af9b65acae49d4099f2c30c520e76f9b757cc6
David Lewin <dlewin@baylibre.com>
2018-01-09 15:52:57 +0100

    new bash script for Lavadocker

---
+++ powerman.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ "$#" -ne 2 ]
+ then
+    echo "Wrong parameters : $#"
+else
+    while [ -n "$1" ]
+    do
+        case "$1" in
+            -off) relay="$2" 
+            COMMAND="off"
+            shift ;;
+            -on) relay="$2" 
+            COMMAND="on"
+            shift ;;
+            -reset) relay="$2" 
+            COMMAND="reset"
+            shift ;;
+            *) echo "Option $1 not recognized";;
+        esac
+        shift
+    done
+
+    python3 /usr/local/bin/ci-client.py $COMMAND -c nrc.cfg -u bbb1 -r $relay 
+fi
+
