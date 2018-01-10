61931848678911fc7cef3ff600c626cff7953030
David Lewin <dlewin@baylibre.com>
2018-01-10 15:00:39 +0100

    chaging readme for formatting

--- powerman.sh
+++ powerman.sh
@@ -1,28 +1,38 @@
 #!/bin/bash
 
-if [ "$#" -ne 2 ]
- then
-    echo "Wrong parameters : $#"
-else
-    while [ -n "$1" ]
-    do
-        case "$1" in
-            -off) relay="$2" 
-            COMMAND="off"
-            shift ;;
-            -on) relay="$2" 
-            COMMAND="on"
-            shift ;;
-            -reset) relay="$2" 
-            COMMAND="reset"
-            shift ;;
-            *) echo "Option $1 not recognized";;
-        esac
-        shift
-    done
 
-    echo "cd /usr/local/bin/"
-    echo "ls"    
-    echo "python3 ci-client.py $COMMAND -c nrc.cfg -u bbb1 -r $relay"
+reset_board()
+{
+    print "python3 ci-client.py off -c nrc.cfg -u bbb1 -r $1"
+    sleep 5
+    print "python3 ci-client.py on -c nrc.cfg -u bbb1 -r $1"
+}
+
+
+while [ -n "$1" ]
+do
+    case "$1" in
+        -off) relay="$2" 
+        COMMAND="off"
+        shift ;;
+        -on) relay="$2" 
+        COMMAND="on"
+        shift ;;
+        -reset) relay="$2" 
+        COMMAND="reset"
+        shift ;;
+        *) echo "Option $1 not recognized";;
+    esac
+    shift
+done
+
+cd /usr/local/bin/
+ls   
+
+if [ $COMMAND = "reset" ]
+then
+    reset_board $relay
+else
+    python3 ci-client.py $COMMAND -c nrc.cfg -u bbb1 -r $relay
 fi
 
